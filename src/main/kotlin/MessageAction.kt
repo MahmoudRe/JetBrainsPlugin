714bbfc73cdc61b7dec9562d3d1fb30ba20f90cc
Cheyenne <cheyenne118312@gmail.com>
2020-04-13 16:02:06 +0200

    [feat] Make action availabilty context dependent

--- src/main/kotlin/MessageAction.kt
+++ src/main/kotlin/MessageAction.kt
@@ -1,5 +1,6 @@
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.Messages
 
 /**
@@ -7,7 +8,18 @@ import com.intellij.openapi.ui.Messages
  */
 class MessageAction : AnAction() {
 
+    /**
+     * Displays a message dialog.
+     */
     override fun actionPerformed(e: AnActionEvent) {
         Messages.showMessageDialog(e.project, "Hello!", "Message dialog", Messages.getInformationIcon())
+    }
+
+    /**
+     * Determines the availability of this menu item.
+     */
+    override fun update(e: AnActionEvent) {
+        val project: Project? = e.project
+        e.presentation.isEnabledAndVisible = project != null
     }
 }
