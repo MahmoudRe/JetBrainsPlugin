d077394f9bd3e9014b575344d67ffaa59a1baf19
jasonbloom <j.s.j.g.bloom@student.tudelft.nl>
2020-04-13 22:38:55 +0200

    [feature] Traversing the current document inside the sandbox editor

--- src/main/kotlin/MessageAction.kt
+++ src/main/kotlin/MessageAction.kt
@@ -1,7 +1,10 @@
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.LangDataKeys
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.Messages
+import com.intellij.psi.*
+
 
 /**
  * This Action creates a message dialog.
@@ -12,7 +15,17 @@ class MessageAction : AnAction() {
      * Displays a message dialog.
      */
     override fun actionPerformed(e: AnActionEvent) {
+
         Messages.showMessageDialog(e.project, "Hello!", "Message dialog", Messages.getInformationIcon())
+
+        val file : PsiFile = PsiDocumentManager.getInstance(e.project!!).getPsiFile(e.getData(LangDataKeys.EDITOR)?.document!!)!!
+
+        file.accept(object : JavaRecursiveElementVisitor() {
+            override fun visitMethod(method: PsiMethod?) {
+                super.visitMethod(method)
+                print(method.toString())
+            }
+        })
     }
 
     /**
