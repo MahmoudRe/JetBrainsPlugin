5d2f73d27ffbf0fc11662e70b5772eb3e3f35fe1
Cheyenne <cheyenne118312@gmail.com>
2020-04-14 13:52:54 +0200

    [feat] Write file to disk
    The plugin now collects information on methods and writes it to a file, which is then stored on disk.
    Also, a pop-up message will warn against creating duplicate reports for class.
    
    GitHub issue #8

--- src/main/kotlin/MethodInspector.kt
+++ src/main/kotlin/MethodInspector.kt
@@ -1,14 +1,16 @@
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.LangDataKeys
+import com.intellij.openapi.application.Application
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.fileTypes.PlainTextLanguage
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.Messages
 import com.intellij.psi.*
-import java.io.File
 
 
 /**
- * This Action creates a message dialog.
+ * This action performs an analysis of the class methods.
  */
 class MethodInspector : AnAction() {
 
@@ -17,29 +19,56 @@ class MethodInspector : AnAction() {
      */
     override fun actionPerformed(e: AnActionEvent) {
 
-        print("Reading file \n \n")
+        Messages.showMessageDialog(e.project, "Your report is being generated", "Method Inspector", Messages.getInformationIcon())
 
+        // Create a PsiFile from the currently active class.
         val file : PsiFile = PsiDocumentManager.getInstance(e.project!!).getPsiFile(e.getData(LangDataKeys.EDITOR)?.document!!)!!
 
+        // Initialize the StringBuilder
+        var builder : StringBuilder = StringBuilder("# Method Analyzer Report \n")
+        builder.append("## Class: ${file.name} \n-----\n \n")
+
+        //Iterate over the class methods
         file.accept(object : JavaRecursiveElementVisitor() {
             override fun visitMethod(method: PsiMethod?) {
-                //super.visitMethod(method)
                 if (method != null) {
-                    File("report.txt").writeText(method.name + "/n")
-                    print("Method " + method.name + ": ")
+                    // Save the method name.
+                    builder.append("### Method ${method.name}: ")
+
+                    // Check the return type.
                     val returnType = method.returnType
-                    if (returnType == null) print ("(constructor) \n") else {
-                        print("\n   Return type: " + returnType.getPresentableText(true) + "\n") }
-                    print("   Parameter list: " + method.parameterList.text + "\n")
+                    if (returnType == null){
+                        builder.append("(constructor) \n")
+                    } else {
+                        builder.append("\n   Return type: ")
+                        builder.append(returnType.getPresentableText(true))
+                        builder.append("\n") }
+
+                    // Save the parameter list.
+                    builder.append("   Parameter list: ${method.parameterList.text} \n")
+
+                    //Save the thrown exceptions.
                     val list = method.throwsList.text
-                    if (list.isNotEmpty()) print("   Throws: $list")
-                    print("\n \n")
+                    if (list.isNotEmpty()) builder.append("   Throws: $list")
+
+                    builder.append("\n \n")
                 }
             }
         })
 
-        Messages.showMessageDialog(e.project, "Your report is being generated!", "Method Inspector", Messages.getInformationIcon())
+        // Write the results to a file.
+        val factory = PsiFileFactory.getInstance(e.project)
+        var result = factory.createFileFromText("report-${file.name}.md", PlainTextLanguage.INSTANCE, builder.toString())
 
+        // Write the generated file to disk. Duplicate reports are prohibited.
+        val application : Application = ApplicationManager.getApplication()
+        val directory : PsiDirectory = LangDataKeys.IDE_VIEW.getData(e.getDataContext())!!.getOrChooseDirectory()!!
+        application.runWriteAction(Runnable {
+            try {
+                result = directory.add(result) as PsiFile?}
+            catch (ex : Exception) {
+                Messages.showMessageDialog(e.project, "A report for this class already exist", "Warning", Messages.getWarningIcon())
+        }})
     }
 
     /**
